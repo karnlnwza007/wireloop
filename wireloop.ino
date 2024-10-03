895924b27013b046243697e96f429886b27a88ed
karnlnwza007 <karnmanopas@gmail.com>
2024-10-03 16:21:51 +0700

    Add files via upload
    version2

--- wireloop.ino
+++ wireloop.ino
@@ -2,13 +2,13 @@
 #include <LiquidCrystal_I2C.h>
 
 #define dirPin 8       // Control direction (high = forward) / (low = backward)
-#define stepPin 9      // Pulse pin to drive motor
+#define stepPin 12     // Pulse pin to drive motor
 #define enablePin 10   // (low = on) / (high = off)
 #define homeSwitch 7   // Home position pin (pull-up)j
-#define endSwitch 2    // End position pin (pull-up)
+#define endSwitch 3    // End position pin (pull-up)
 #define startButton 4  // Start button
-#define touchPin 3     // Touch sensor pin
-#define signalPIN 6    // Signal for speaker
+#define touchPin 2     // Touch sensor pin
+#define speakerPin 6   // Signal for speaker
 
 uint16_t touchCount = 0;              // Number of touches counted
 const int stepDelay = 500;            // Adjust to increase/decrease motor speed (in microseconds)
@@ -20,11 +20,24 @@ bool goingHome = false;             // Flag for motor returning to home
 unsigned long currentTime = 0;
 unsigned long lastTouchTime = 0;         // Last touch timestamp
 const unsigned long debounceTime = 400;  // Debounce interval for touch detection (400 ms)
+const unsigned long speakerTime = 200;
+uint8_t timeLimit = 30;
+unsigned long startTime = 0;
+uint8_t remainingTime = timeLimit;
+unsigned long nextStep = 0;
+bool speakerOn = false;
 
 LiquidCrystal_I2C lcd(0x3f, 16, 2);  // Declare i2c LCD
+void moveToHome();
+void moveToEnd();
+void updateLCD(const char* status, uint16_t count);
+void updateValue(uint16_t count);
+void updateTime(uint8_t time);
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+  Serial.println("Board Start");
   pinMode(dirPin, OUTPUT);
   pinMode(stepPin, OUTPUT);
   pinMode(enablePin, OUTPUT);
@@ -32,32 +45,26 @@ void setup() {
   pinMode(endSwitch, INPUT_PULLUP);
   pinMode(touchPin, INPUT_PULLUP);
   pinMode(startButton, INPUT_PULLUP);
-  pinMode(signalPIN, INPUT);
+  pinMode(speakerPin, OUTPUT);
+  randomSeed(analogRead(0));
   lcd.begin();
   lcd.backlight();
   // Move the motor to home position initially
   moveToHome();
   // Show the initial status on the LCD
-  updateLCD("Stopped", touchCount);
+  updateLCD("Stop", touchCount);
 }
 
 void loop() {
   if (digitalRead(startButton) == LOW) {
     Serial.println("Start Button Detected!");
-    if (gameRunning) {
-      // If the game is running and start button is pressed, stop the game and go back home
-      Serial.println("Stopping game");
-      gameRunning = false;
-      moveToHome();
-    } else {
-      // Start the game
-      Serial.println("Starting game");
-      delay(1000);
-      gameRunning = true;
-      touchCount = 0;  // Reset the touch count
-      updateLCD("Running", touchCount);
-      moveToEnd();  // Move towards the end position
-    }
+    // Start the game
+    Serial.println("Starting game");
+    delay(1000);
+    gameRunning = true;
+    touchCount = 0;  // Reset the touch count
+    updateLCD("Run", touchCount);
+    moveToEnd();  // Move towards the end position
   }
 }
 
@@ -72,7 +79,7 @@ void handleTouch() {
 // Function to move the motor towards the home position
 void moveToHome() {
   Serial.println("Going back to home position");
-  updateLCD("Stopped", touchCount);
+  updateLCD("Stop", touchCount);
   digitalWrite(enablePin, LOW);  // Enable motor
   digitalWrite(dirPin, LOW);     // Set direction to backward (towards home)
 
@@ -80,50 +87,121 @@ void moveToHome() {
     // Drive the motor until homeSwitch is pressed (LOW)
     stepMotor();
   }
-
+  Serial.println("Reached Home position");
   digitalWrite(enablePin, HIGH);  // Disable motor when home is reached
 }
 
 // Function to move the motor towards the end position
-void moveToEnd() {
+void moveToEnd() {               // game running
   digitalWrite(enablePin, LOW);  // Enable motor
-  digitalWrite(dirPin, HIGH);    // Set direction to forward (towards end)
+  //set time variable
+  startTime = millis();
+  remainingTime = timeLimit;
+  nextStep = 1000;
   attachInterrupt(digitalPinToInterrupt(touchPin), handleTouch, LOW);
-
-
-  while (gameRunning && digitalRead(endSwitch) == HIGH) {
-
-    if (interruptOff) {
+  // set flag if game hit endSwitch
+  bool endSwitchFlag = false;
+  //set variable for counting
+  uint16_t step = 8000;
+  uint8_t direction = 1;            // 1 = forward , 0 = backward
+  digitalWrite(dirPin, direction);  // Set direction to forward (towards end)
+  while (true) {
+    //for loop to drive motor in step and by direction set
+    for (uint16_t i = 0; i < step; i++) {
+      //in for loop do 6 things
+      //1. check time to update LCD for time remaining
       currentTime = millis();
-      if (currentTime - lastTouchTime >= debounceTime) {
-        interruptOff = false;
-        attachInterrupt(digitalPinToInterrupt(touchPin), handleTouch, LOW);
+      if ((currentTime - startTime) >= nextStep) {
+        remainingTime--;
+        nextStep += 1000;
+        updateTime(remainingTime);
+        if (remainingTime == 0) {  // timeout break and end game
+          gameRunning = false;
+          break;
+        }
+      }
+      //2. check duration to enable touch pin again
+      if (interruptOff) {
+        if (currentTime - lastTouchTime >= debounceTime) {
+          interruptOff = false;
+          attachInterrupt(digitalPinToInterrupt(touchPin), handleTouch, LOW);
+        }
+      }
+      //handle speaker on for interval
+      if (speakerOn) {
+        if (currentTime - lastTouchTime >= speakerTime) {
+          digitalWrite(speakerPin, LOW);
+          speakerOn = false;
+        }
+      }
+      //3.handle touchpin if value from interrupt is flagged
+      if (touchDetected) {
+        touchCount++;
+        updateValue(touchCount);
+        lastTouchTime = millis();
+        interruptOff = true;
+        touchDetected = false;
+        speakerOn = true;
+        digitalWrite(speakerPin, HIGH);
+      }
+      //4.drive motor
+      stepMotor();
+      //5.Check for start button press to stop the game
+      if (digitalRead(startButton) == LOW) {
+        detachInterrupt(digitalPinToInterrupt(touchPin));
+        gameRunning = false;
+        updateLCD("Stopped", touchCount);
+        break;
+      }
+      //6. if end switch is hit, go backward for 3000 step
+      if (digitalRead(endSwitch) == LOW) {
+        step = 3000;
+        i = 0;
+        endSwitchFlag = true;
+        direction = 0;
+        digitalWrite(dirPin, direction);
       }
     }
-    if (touchDetected && gameRunning) {
-      touchCount++;
-      updateValue(touchCount);
-      lastTouchTime = millis();
-      interruptOff = true;
-      //detachInterrupt(digitalPinToInterrupt(touchPin));
-      touchDetected = false;
-    }
-    stepMotor();
-
-    // Check for start button press to stop the game
-    if (digitalRead(startButton) == LOW) {
-      detachInterrupt(digitalPinToInterrupt(touchPin));
-
-      gameRunning = false;
-      updateLCD("Stopped", touchCount);
-      moveToHome();
+    if (!gameRunning) {
       break;
     }
+    //set opposite direction and random step value
+    if (direction == 1) {
+      direction = 0;
+      step = random(2500, 3500);
+      digitalWrite(dirPin, direction);
+      Serial.print("Going backward. Step is : ");
+      Serial.println(step);
+    } else {
+      direction = 1;
+      if (endSwitchFlag) {
+        endSwitchFlag = false;
+        // fix delay this flag
+        step = 6000;
+      } else {
+        step = (step * 6) / 5;
+      }
+      digitalWrite(dirPin, direction);
+      Serial.print("Going hforward. Step is : ");
+      Serial.println(step);
+    }
   }
-  detachInterrupt(digitalPinToInterrupt(touchPin));
-  digitalWrite(enablePin, HIGH);  // Disable motor when end is reached
+  //reset state
+  digitalWrite(speakerPin, LOW);
 
-  delay(2000);
+  // beep signal to indicate time over
+  digitalWrite(speakerPin, HIGH);
+  delay(1000);
+  digitalWrite(speakerPin, LOW);
+  delay(500);
+  speakerOn = false;
+  if (interruptOff) {
+    interruptOff = false;
+  } else {
+    detachInterrupt(digitalPinToInterrupt(touchPin));
+  }
+  touchDetected = false;
+  delay(1000);
   moveToHome();
 }
 
@@ -139,13 +217,25 @@ void stepMotor() {
 void updateLCD(const char* status, uint16_t count) {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Status: ");
   lcd.print(status);
+  lcd.setCursor(6, 0);
+  lcd.print("Time:");
+  lcd.setCursor(11, 0);
+  lcd.print(remainingTime);
   lcd.setCursor(0, 1);
-  lcd.print("Count hit = ");
   lcd.print(count);
 }
 void updateValue(uint16_t count) {
-  lcd.setCursor(12, 1);
+  lcd.setCursor(0, 1);
   lcd.print(count);
+}
+
+void updateTime(uint8_t time) {
+  lcd.setCursor(11, 0);
+  if (time >= 10) {
+    lcd.print(time);
+  } else {
+    lcd.print("0");
+    lcd.print(time);
+  }
 }
